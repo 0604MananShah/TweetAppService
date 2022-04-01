e9609a052a63a3b079ca1b59f4a6b90ae028c036
Shah Manan <32126724+0604MananShah@users.noreply.github.com>
2022-04-01 10:58:19 +0530

    updated KafkaProducerConfig

--- src/main/java/com/tweetapp/configuration/KafkaProducerConfig.java
+++ src/main/java/com/tweetapp/configuration/KafkaProducerConfig.java
@@ -5,6 +5,7 @@ import java.util.Map;
 
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.core.DefaultKafkaProducerFactory;
@@ -16,10 +17,14 @@ import lombok.Generated;
 @Configuration
 @Generated
 public class KafkaProducerConfig {
+
+	@Value(value = "${kafka.bootStrapServer}")
+	private String bootStrapServer;
+
 	@Bean
 	public ProducerFactory<String, String> producerFactory() {
 		Map<String, Object> configProps = new HashMap<>();
-		configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+		configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootStrapServer);
 		configProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
 		configProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
 		return new DefaultKafkaProducerFactory<>(configProps);
